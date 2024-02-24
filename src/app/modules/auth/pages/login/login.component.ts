805878b15286a18f1c593b9f046228e2672712b6
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2024-02-24 12:39:34 +0100

    add reactive form to login page

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required, Validators.max(50)]),
+    password: new FormControl('', [Validators.required, Validators.min(8)])
+  })
 
+  submit() {
+    console.log(this.loginForm.value)
+  }
 }
---
+++ src/app/shared/services/abstract-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {_Service} from "./_service";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AbstractServiceService<T> implements _Service<T>{
+
+  constructor(private _http: HttpClient) { }
+
+  // todo: fix this generic service
+
+  create(item: T): Observable<T> {
+  }
+
+  delete(id: string): Observable<void> {
+    return undefined;
+  }
+
+  getAll(): Observable<T[]> {
+    return undefined;
+  }
+
+  getById(id: string): Observable<T> {
+    return undefined;
+  }
+
+  update(id: string, item: T): Observable<T> {
+    return undefined;
+  }
+}
